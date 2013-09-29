4fb3a32cec0c7048a4a0133a758a6d053e443d92
c9s <cornelius.howl@gmail.com>
2013-09-29 12:33:49 +0800

    enable java i18n filter.

--- render.php
+++ render.php
@@ -4,6 +4,8 @@ require_once 'vendor/autoload.php';
 use Corneltek\Preview\TaskRunner;
 use Corneltek\Preview\ConfigLoader;
 
+define('ENABLE_JAVA_I18N_FILTER', true);
+
 $config = new ConfigLoader;
 $config->loadFileIfExists('config/preview.yml');
 
