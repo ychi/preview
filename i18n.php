4112c99545ad785dbfe1e177a6ba1c59f3122f6d
c9s <cornelius.howl@gmail.com>
2013-12-11 20:29:25 +0800

    Add i18n script

--- i18n.php
+++ i18n.php
@@ -1,9 +1,5 @@
 <?php
 require 'vendor/autoload.php';
-
 use Corneltek\Preview\Task\CompileMessageTask;
-
 $task = new CompileMessageTask(array('paths' => 'design/design/zh_TW', 'flush' => true ));
 $task->run();
-
-
