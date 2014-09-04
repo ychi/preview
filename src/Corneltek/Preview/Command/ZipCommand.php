1a59aa9d5721254bc65b0c0461afed4fbf85c5f8
c9s <yoanlin93@gmail.com>
2014-09-04 20:24:47 +0800

    Add zip command fallback support

--- src/Corneltek/Preview/Command/ZipCommand.php
+++ src/Corneltek/Preview/Command/ZipCommand.php
@@ -22,45 +22,49 @@ class ZipCommand extends Command
     }
 
     public function execute() {
-
-        if (!extension_loaded('zip')) {
-            return $this->logger->error('php zip extension is not installed or enabled.');
-        }
-        if ( !class_exists('ZipArchive')) {
-            return $this->logger->error('ZipArchive class does not exist.');
+        $zipBin = null;
+        if (!extension_loaded('zip') ||  !class_exists('ZipArchive')) {
+            $this->logger->warn('php zip extension is not installed or enabled. fallback to zip command');
+            if ($zipBin = futil_findbin('zip')) {
+                $this->logger->info("$zipBin found.");
+            } else {
+                return $this->logger->error("$zipBin not found.");
+            }
         }
 
         $render = $this->createCommand('\Corneltek\Preview\Command\RenderCommand');
         $render->setOptions($this->getOptions());
         $render->execute();
 
+        $previewDir = 'preview';
         $zipFilename = sprintf("preview_%s.zip", date('Y-m-d') );
-
         if (file_exists($zipFilename)) {
             unlink($zipFilename);
         }
 
 
         $this->logger->info('Creating zip file...');
-        $zip = new ZipArchive();
-
-        if ($zip->open($zipFilename, ZipArchive::CREATE)!==TRUE) {
-            exit("cannot open <$zipFilename>\n");
-        }
-
-        $path = 'preview';
-        $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path, FilesystemIterator::SKIP_DOTS | FilesystemIterator::FOLLOW_SYMLINKS), RecursiveIteratorIterator::SELF_FIRST);
-        foreach($files as $name => $file){
-            $this->logger->info("Adding to zip file: " . $name);
-            if ($file->isFile()) {
-                $zip->addFile($name);
-            } elseif($file->isDir()) {
-                $zip->addEmptyDir($name);
+        if ($zipBin) {
+            system("$zipBin -r $zipFilename $previewDir");
+        } else {
+            $zip = new ZipArchive();
+            if ($zip->open($zipFilename, ZipArchive::CREATE)!==TRUE) {
+                exit("cannot open <$zipFilename>\n");
             }
-        }
 
-        $this->logger->info("Number of files: " . $zip->numFiles);
-        $this->logger->info("Status: " . $zip->status);
-        $zip->close();
+            $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($previewDir, FilesystemIterator::SKIP_DOTS | FilesystemIterator::FOLLOW_SYMLINKS), RecursiveIteratorIterator::SELF_FIRST);
+            foreach($files as $name => $file){
+                $this->logger->info("Adding to zip file: " . $name);
+                if ($file->isFile()) {
+                    $zip->addFile($name);
+                } elseif($file->isDir()) {
+                    $zip->addEmptyDir($name);
+                }
+            }
+
+            $this->logger->info("Number of files: " . $zip->numFiles);
+            $this->logger->info("Status: " . $zip->status);
+            $zip->close();
+        }
     }
 }
